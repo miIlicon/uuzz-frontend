a397964fde76eb49ca92c9a74d4e80456cc196d8
klmhyeonwooo <gentlemonster77@naver.com>
2023-02-19 17:01:22 +0900

    Feat : 공통 컴포넌트 작성 및 구조 설계 (#5)

---
+++ src/components/Header.tsx
@@ -0,0 +1,75 @@
+/** @jsxImportSource @emotion/react */
+import React from 'react';
+import Logo from '../images/logo.png';
+import { css } from '@emotion/react';
+import { Link } from 'react-router-dom';
+import { WrapperProps } from '../@types/type';
+
+const NavTabRight = (props: WrapperProps) => {
+    return (
+        <div css={css`
+            display: flex;
+            column-gap: 4em;
+            margin-right: auto;
+            margin-left: 6em;
+        `} {...props}>
+            {props.children}
+        </div>
+    )
+}
+
+const NavTabLeft = (props: WrapperProps) => {
+    return (
+        <div css={css`
+            display: flex;
+            column-gap: 4em;
+            margin-left: auto;
+            margin-right: 8em;
+        `} {...props}>
+            {props.children}
+        </div>
+    )
+}
+
+export default function Header() {
+    return (
+        <header css={css`
+            width: 100%;
+            height: 6em;
+            // background-color: gray;
+            padding-top: .5rem;
+            padding-bottom: .5rem;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-size: 0.9rem;
+            font-family: 'Nanum Myeongjo';
+
+            a {
+                text-decoration: none;
+                letter-spacing: -0.03em;
+                color: #333d4b;
+                transition: .4s all;
+
+                &:hover {
+                    opacity: 70%;
+                }
+            }
+        `}>
+            <NavTabRight>
+                <Link to="">독립서점 홈</Link>
+                <Link to="">주인장의 일상</Link>
+                <Link to="">우리 동네 독립서점</Link>
+            </NavTabRight>
+            <img src={Logo} alt="로고 이미지" css={css`
+                position: absolute;
+                width: 11rem;
+                `} />
+            <NavTabLeft>
+                <Link to="">마이 페이지</Link>
+                <Link to="">글쓰기</Link>
+            </NavTabLeft>
+
+        </header>
+    )
+}
